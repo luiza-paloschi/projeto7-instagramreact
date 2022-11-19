2ea73dc228d5a88ab76b7f17812caf00bb570c30
luiza-paloschi <luizapaloschi@gmail.com>
2022-11-19 00:50:12 -0300

    alterar imagem de perfil e nome

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,11 +1,31 @@
+import { useState } from "react"
+
 export default function Usuario(){
-    return <div class="usuario">
-    <img src="assets/img/catanacomics.svg" />
+    
+    const [name, setName] = useState("Catana");
+    const [imgProfile, setImgProfile] = useState("assets/img/catanacomics.svg");
+
+    function changeName(){
+        const resposta = prompt("Digite o novo nome");
+        if(resposta!== null && resposta!==undefined && resposta!==""){
+         setName(resposta);
+        }
+    }
+
+    function changeImage(){
+      const imagem = prompt("Digite o link da imagem");
+      if(imagem!== null && imagem!==undefined && imagem!==""){
+       setImgProfile(imagem);
+      }
+  }
+
+    return <div class="usuario" data-test="user">
+    <img data-test="profile-image" onClick={changeImage} src={imgProfile} alt=""/>
     <div class="texto">
       <strong>catanacomics</strong>
-      <span>
-        Catana
-        <ion-icon name="pencil"></ion-icon>
+      <span data-test="name">
+        {name}
+        <ion-icon data-test="edit-name" onClick={changeName} name="pencil"></ion-icon>
       </span>
     </div>
   </div>
